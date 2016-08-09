5a443f430000e81352a5c3d6f721e8afe6b5d09c
brandontiqui <btiqui@gmail.com>
2016-08-08 20:07:56 -0700

    Allow user to add rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,9 +9,10 @@ app.server = 'https://api.parse.com/1/classes/messages';
 app.messageStorage = {};
 app.queueMessage = [];
 app.friendList = {};
-app.rooms = {};
+app.rooms = { 'Main Room': true};
 
 app.init = function() {
+  app.fetch();
   console.log('init');
 };
 app.send = function(message) {
@@ -43,11 +44,17 @@ app.fetch = function() {
           var newMessage = new Message(ParseObj.username, ParseObj.text, ParseObj.roomname);
           app.messageStorage[ParseObj.objectId] = newMessage;
           app.queueMessage.push(newMessage);
+          if (!app.rooms[ParseObj.roomname]) {
+            app.rooms[ParseObj.roomname] = true;
+            // app.addRoom(ParseObj.roomname.replace(/ /g, ''));
+            app.addRoom(ParseObj.roomname);
+          }
         }
       });
       for (var elem in app.queueMessage) {
         app.addMessage(app.queueMessage.pop());
       }
+
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -66,7 +73,7 @@ app.addMessage = function(message) {
   var msg = $('<div class="message"></div>');
   var username = $('<span class="username"></span>');
   var userMessage = $('<span class="text"></span>');
-  msg.addClass(message.roomname);
+  msg.addClass(message.roomname ? message.roomname.replace(/ /g, '') : 'defaultToAll');
   username.text(message.username);
   username.addClass(message.username);
   if (app.friendList.hasOwnProperty(String(message.username))) {
@@ -80,6 +87,9 @@ app.addMessage = function(message) {
   });
   msg.append(username).append($('<br>')).append(userMessage);
   $('#chats').prepend(msg);
+  if (!app.canAdd(message.roomname)) {
+    msg.hide();
+  }
   return message;
 };
 app.addRoom = function(roomname) {
@@ -100,15 +110,41 @@ app.handleSubmit = function() {
 $(document).ready(function() {
   $('#input').keydown(function(e) {
     if (e.keyCode === 13 && this.val()) {
-      var message = new Message(window.location.search.substring(10), this.val(), 'PLACEHOLDER');
+      var message = new Message(window.location.search.substring(10), this.val(), $('#roomSelect').val());
       app.send(app.addMessage(message));
       this.val('');
     }
   }.bind($('#input')));
 
+  $('#room-maker').keydown(function(e) {
+    if (e.keyCode === 13 && this.val()) {
+      app.addRoom(this.val());
+      this.val('');
+    }
+  }.bind($('#room-maker')));
+
   $('#send').on('submit', function() {
     app.handleSubmit();
   });
+
+  $('#roomSelect').on('change', function(e) {
+    if ($(roomSelect).val() === 'All') {
+      $('#chats').children().show();
+    } else {
+      $('#chats').children().hide();
+      $('.' + $(roomSelect).val()).show();
+    }
+  });
+
+  app.canAdd = function(messageRoom) {
+    if ($('#roomSelect').val() === 'All') {
+      return true;
+    } else if ($('roomSelect').val() === messageRoom) {
+      return true;
+    }
+    return false;
+  };
+
 
   setInterval(function() {
     app.fetch();
